b5652ed681531485d2d5cba6ed0e503d8fa56159
aditditkudit <adityaadit92@gmail.com>
2019-08-28 10:09:13 +0700

    init project again

---
+++ src/test/java/com/tavs/bootcamp/springboottrainingtelkom2019/SpringbootTrainingTelkom2019ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tavs.bootcamp.springboottrainingtelkom2019;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTrainingTelkom2019ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
