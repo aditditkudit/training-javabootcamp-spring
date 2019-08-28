f08846f6d9b4d3424cf5a859a727964ec7beeba4
aditditkudit <adityaadit92@gmail.com>
2019-08-28 10:06:30 +0700

    init project

---
+++ src/main/java/com/tavs/bootcamp/springboottrainingtelkom2019/SpringbootTrainingTelkom2019Application.java
@@ -0,0 +1,13 @@
+package com.tavs.bootcamp.springboottrainingtelkom2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrainingTelkom2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrainingTelkom2019Application.class, args);
+	}
+
+}
