d2e3d0b8685b9f2c2bff3b951951fb9684c95644
aditditkudit <adityaadit92@gmail.com>
2019-08-29 09:31:52 +0700

    menggunakan thymeleaf

--- src/main/java/com/tavs/bootcamp/springboottrainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/tavs/bootcamp/springboottrainingtelkom2019/controller/HaloController.java
@@ -1,12 +1,19 @@
 package com.tavs.bootcamp.springboottrainingtelkom2019.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HaloController {
+
+    //Menggunakan protokol Get
     @GetMapping("/halo")
-    public String halo(){
+    public String halo(Model model){
+        model.addAttribute("tanggal", new Date());
+        model.addAttribute("message", "Kudit gansss");
         return "/belajar-html";
     }
 
