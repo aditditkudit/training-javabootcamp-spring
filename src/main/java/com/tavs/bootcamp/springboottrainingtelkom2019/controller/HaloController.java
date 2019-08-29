c05655d7f7e34b425ae0c0c5f621d7d0d8396df9
aditditkudit <adityaadit92@gmail.com>
2019-08-29 09:47:57 +0700

    update request param & insert it into form

--- src/main/java/com/tavs/bootcamp/springboottrainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/tavs/bootcamp/springboottrainingtelkom2019/controller/HaloController.java
@@ -3,6 +3,7 @@ package com.tavs.bootcamp.springboottrainingtelkom2019.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 
@@ -11,9 +12,14 @@ public class HaloController {
 
     //Menggunakan protokol Get
     @GetMapping("/halo")
-    public String halo(Model model){
+    public String halo(Model model,
+                       @RequestParam(required = false) String nama, //Menggunakan Request parameter
+                       @RequestParam(required = false) Integer umur){
         model.addAttribute("tanggal", new Date());
         model.addAttribute("message", "Kudit gansss");
+        model.addAttribute("nama", nama); // Memasukkan request parameter pada variable name di html
+        model.addAttribute("umur", umur); // Memasukkan request parameter pada variable umur di html
+        System.out.println("Nama dari request: " + nama + ", umur dari request: "+ umur);
         return "/belajar-html";
     }
 
